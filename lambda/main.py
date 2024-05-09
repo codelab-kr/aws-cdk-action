9710c5cba7a263f0614ead7d80083fa284060226
codelab-kr <master@code-lab.kr>
2024-05-09 20:27:04 +0900

    change message

--- lambda/main.py
+++ lambda/main.py
@@ -4,7 +4,7 @@ import os
 def handler(event, context):
     version = os.getenv("VERSION", "0.0")
     response_body = {
-        "message": "Hello, World!",
+        "message": "Hello, World from Lambda! 🚀 ",
         "version": version,
     }
     return {"statusCode": 200, "body": response_body}
